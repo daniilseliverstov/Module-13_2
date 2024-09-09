7c992d11640c07c43b91fd20ce4b95df95e22e37
daniilseliverstov <selivrstov91@internet.ru>
2024-09-09 06:53:52 +0300

    Хендлеры обработки сообщений

---
+++ main.py
@@ -0,0 +1,22 @@
+import asyncio
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+with open("teg_api") as file:
+    api = file.read()
+
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def start_massage(massage):
+    print('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message_handler()
+async def all_massage(massage):
+    print('Введите команду /start, чтобы начать общение.')
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
